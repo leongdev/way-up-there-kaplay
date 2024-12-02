3a12867e457b4b210e2c148041a23c014e78d1dc
leongdev <leongdev@gmail.com>
2024-12-01 18:35:53 -0800

    Starting adding players physics

--- src/objects/player/player.ts
+++ src/objects/player/player.ts
@@ -1,12 +1,16 @@
 import {k} from "../../settings/kaplay";
 import {Objects} from "../../utils/types";
 import {config} from "./config";
+import {GameObj} from "kaplay";
 
 k.loadSprite(
     Objects.PLAYER,
     "sprites/player.png",
     config
 )
+
+const JUMP_FORCE = 800;
+
 export function loadPlayer(posX: number, posY: number) {
     const player =  k.add([
         k.sprite(Objects.PLAYER),
@@ -15,7 +19,18 @@ export function loadPlayer(posX: number, posY: number) {
         area(),
         body()
     ]);
+
+    onKeyPress("space", ()=>{
+        playerJump(player)
+    });
+
     player.play("idle");
 
     return player;
+}
+
+const playerJump = (player: GameObj) => {
+    if (player.isGrounded()) {
+        player.jump(JUMP_FORCE);
+    }
 }
--- src/scenes/initial.ts
+++ src/scenes/initial.ts
@@ -2,10 +2,28 @@ import {Scenes} from "../utils/types";
 import {k} from "../settings/kaplay";
 import {loadPlayer} from "../objects/player/player";
 
+const GRAVITY       = 2400;
+const FLOOR_HEIGHT  = 48;
+
 k.scene(Scenes.INITIAL, ()=> {
 
-    const player = loadPlayer(300,300);
-    k.onClick(() => k.addKaboom(k.mousePos()));
+    //Setup Physics
+    setGravity(GRAVITY);
+
+    //Load Objects
+    loadPlayer(300,300);
+
+    // floor
+    add([
+        rect(width(), FLOOR_HEIGHT),
+        outline(1),
+        pos(0, height()),
+        anchor("botleft"),
+        area(),
+        body({ isStatic: true }),
+        color(255, 255, 255),
+    ]);
+
 })
 
 
